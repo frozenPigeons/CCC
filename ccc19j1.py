3017fc447e327b28abcb8194b1397686ef4eb062
frozenPigeons <118379320+frozenPigeons@users.noreply.github.com>
2022-11-16 22:15:36 -0500

    Create ccc19j1.py

---
+++ ccc19j1.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+
+apples = (a*3)+(b*2)+(c*1)
+bananas = (d*3)+(e*2)+(f*1)
+
+if apples > bananas:
+    print("A")
+if apples < bananas:
+    print("B")
+if apples == bananas:
+    print("T")
